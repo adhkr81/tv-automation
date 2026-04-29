296c0e94f40efa462d924559e69780b07a8bba72
Adriano Harm <adhkr81@gmail.com>
2026-04-29 08:28:52 -0500

    nicholas

--- scripts/topics.mjs
+++ scripts/topics.mjs
@@ -32,7 +32,7 @@ export const topics = {
       ],
     ],
   },
-  "143": {
+  "2": {
     steps: [
       [
         { type: "remote", key: "?" },
@@ -57,46 +57,6 @@ export const topics = {
         { type: "wait", ms: 900 },
       ],
     ],
-  },
-  "200": {
-    steps: [
-      [
-        { type: "remote", key: "?" },
-        { type: "wait", ms: 500 },
-        { type: "remote", key: "▼" },
-        { type: "wait", ms: 700 },
-      ],
-      [
-        { type: "remote", key: "▶" },
-        { type: "wait", ms: 500 },
-        { type: "remote", key: "ENTER" },
-        { type: "wait", ms: 800 },
-      ],
-      [
-        { type: "remote", key: "RETURN" },
-        { type: "wait", ms: 700 },
-      ],
-    ],
-  },
-  "201": {
-    steps: [
-      [
-        { type: "remote", key: "?" },
-        { type: "wait", ms: 500 },
-        { type: "remote", key: "▲" },
-        { type: "wait", ms: 700 },
-      ],
-      [
-        { type: "remote", key: "◀" },
-        { type: "wait", ms: 500 },
-        { type: "remote", key: "ENTER" },
-        { type: "wait", ms: 800 },
-      ],
-      [
-        { type: "remote", key: "EXIT" },
-        { type: "wait", ms: 700 },
-      ],
-    ],
-  },
+  }
 };
 
