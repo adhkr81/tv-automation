93b377d8261ea645c9c4820c641aef668f5681ef
Nicholas Song <nicholasjusong@gmail.com>
2026-04-29 16:59:27 -0500

    Added two topics, 2nd topic menu screen expires. Located KEY_YELLOW, KEY_RED, KEY_RED, no blue key.

---
+++ scripts/topics copy.mjs
@@ -0,0 +1,130 @@
+export const reset = {
+  "0": {
+    skipCapture: true,
+    steps: [
+      [
+        { type: "remote", key: "KEY_CONTENTS" },
+        { type: "wait", ms: 900 },
+      ],
+    ],
+  },
+}
+
+export const topics = {
+  "1": {
+    steps: [
+      [
+        { type: "remote", key: "KEY_CONTENTS" },
+        { type: "wait", ms: 900 },
+      ],
+      [
+        { type: "remote", key: "KEY_LEFT" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+      ],
+      [
+        { type: "remote", key: "KEY_ENTER" },
+        { type: "wait", ms: 900 },
+      ],
+      [
+        { type: "remote", key: "KEY_ENTER" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+      ],
+      [
+        { type: "remote", key: "KEY_ENTER" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_UP" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_UP" },
+        { type: "wait", ms: 900 },
+      ],
+    ],
+  }
+  ,
+  "2": {
+    steps: [
+      [
+        { type: "remote", key: "KEY_CONTENTS" },
+        { type: "wait", ms: 900 },
+      ],
+      [
+        { type: "remote", key: "KEY_LEFT" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+      ],
+      [
+        { type: "remote", key: "KEY_ENTER" },
+        { type: "wait", ms: 1100 },
+      ],
+      [
+        { type: "remote", key: "KEY_ENTER" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 },
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 }
+      ],
+      [
+        { type: "remote", key: "KEY_ENTER" },
+        { type: "wait", ms: 1100 },
+      ],
+      [
+        { type: "remote", key: "KEY_ENTER" },
+        { type: "wait", ms: 1100 },
+      ],
+      [
+        { type: "remote", key: "KEY_DOWN" },
+        { type: "wait", ms: 900 }
+      ],
+      [
+        { type: "remote", key: "KEY_ENTER" },
+        { type: "wait", ms: 1100 },
+      ],
+      [
+        { type: "remote", key: "KEY_ENTER" },
+        { type: "wait", ms: 5000 },
+      ],
+    ],
+  }
+};
+
